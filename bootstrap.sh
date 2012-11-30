6f6a2e7c7be7cc53abca2a01fe4cb5a709905c83
Tobias Kremer <tobias.kremer@gmail.com>
2012-11-30 15:22:57 +0100

    - Fixed tab stops - Added zsh download and install to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,12 @@
-#!/bin/zsh
+#!/bin/sh
 
 DIR="$PWD/`dirname $0`"
+
+echo "Downloading and installing oh-my-zsh..."
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 ln -s $DIR/ack/ackrc ~/.ackrc
 ln -s $DIR/git/gitconfig ~/.gitconfig
 ln -s $DIR/vim/vimrc ~/.vimrc
 ln -s $DIR/vim/gvimrc ~/.gvimrc
 ln -s $DIR/zsh/zshrc ~/.zshrc
-
