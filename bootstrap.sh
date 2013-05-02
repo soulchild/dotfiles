b6a541797cf5a9feb5b8b3b2fb6f976630712eac
Tobias Kremer <tobias.kremer@gmail.com>
2013-05-02 12:07:51 +0200

    - Move default zshrc out of the way. - Symlink vim color scheme. - Remove local::lib eval.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,11 @@ ln -s $DIR/ack/ackrc ~/.ackrc
 ln -s $DIR/git/gitconfig ~/.gitconfig
 ln -s $DIR/vim/vimrc ~/.vimrc
 ln -s $DIR/vim/gvimrc ~/.gvimrc
+mv ~/.zshrc ~/.zshrc_default
 ln -s $DIR/zsh/zshrc ~/.zshrc
 ln -s $DIR/perltidy/perltidyrc ~/.perltidyrc
+
+mkdir ~/.vim/colors
+ln -s ~/.dotfiles/vim/ir_black.vim ~/.vim/colors/
+
+curl -L http://cpanmin.us | perl - --sudo App::cpanminus
