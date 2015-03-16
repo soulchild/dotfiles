3b1975ce4fa3158036b66667bd4d06875a0fad68
Tobias Kremer <tobias.kremer@gmail.com>
2015-03-16 11:38:52 +0100

    Replaced Oh-my-zsh with prezto.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,22 +2,38 @@
 
 DIR="$PWD/`dirname $0`"
 
-echo "Downloading and installing oh-my-zsh..."
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+echo "Downloading and installing prezto..."
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 echo "Cloning vundle..."
 git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
 
+echo "Cloning z..."
+git clone https://github.com/rupa/z.git ~/.dotfiles/
+
+# ack
 ln -s $DIR/ack/ackrc ~/.ackrc
+
+# git
 ln -s $DIR/git/gitconfig ~/.gitconfig
+
+# vim
 ln -s $DIR/vim/vimrc ~/.vimrc
 ln -s $DIR/vim/gvimrc ~/.gvimrc
-mv ~/.zshrc ~/.zshrc_default
-ln -s $DIR/zsh/zshrc ~/.zshrc
-ln -s $DIR/perltidy/perltidyrc ~/.perltidyrc
 
+# zsh/prezto
+mv ~/.zshrc ~/.zshrc_default # backup
+ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
+ln -s $DIR/zsh/zshrc ~/.zshrc
+ln -s $DIR/zsh/zpreztorc ~/.zpreztorc
+ln -s $DIR/zsh/zprofile ~/.zprofile
+
+# terminal colors
 mkdir ~/.vim/colors
 ln -s ~/.dotfiles/vim/ir_black.vim ~/.vim/colors/
 
+# perl
+ln -s $DIR/perltidy/perltidyrc ~/.perltidyrc
 curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 cpanm --sudo local::lib
