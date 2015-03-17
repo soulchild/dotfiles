5803c6365b721d9a1adcdf8b605b2e824ff22fde
Tobias Kremer <tobias.kremer@gmail.com>
2015-03-17 10:08:19 +0100

    Added mk_symlink() function to silence warnings and errors if the script is run again.
    Added homebrew.
    
    Added fasd.
    
    Changed path order.
    
    Re-added rvm support.
    
    Changed key-bindings to emacs.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 DIR="$PWD/`dirname $0`"
+cd $DIR
+
+function mk_symlink {
+    if [ ! -z "$1" ] & [ ! -z "$2" ]
+        then
+            echo "Symlinking $1 -> $2"
+            ln -s $1 $2 >/dev/null 2>&1
+        else
+            echo "mk_symlink() missing arguments."
+    fi
+}
 
 echo "Downloading and installing prezto..."
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -11,29 +22,37 @@ git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
 echo "Cloning z..."
 git clone https://github.com/rupa/z.git ~/.dotfiles/
 
+echo "Installing Homebrew..."
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
+
+echo "Installing FASD..."
+brew install fasd
+
 # ack
-ln -s $DIR/ack/ackrc ~/.ackrc
+mk_symlink $DIR/ack/ackrc ~/.ackrc
 
 # git
-ln -s $DIR/git/gitconfig ~/.gitconfig
+mk_symlink $DIR/git/gitconfig ~/.gitconfig
 
 # vim
-ln -s $DIR/vim/vimrc ~/.vimrc
-ln -s $DIR/vim/gvimrc ~/.gvimrc
+mk_symlink $DIR/vim/vimrc ~/.vimrc
+mk_symlink $DIR/vim/gvimrc ~/.gvimrc
 
 # zsh/prezto
 mv ~/.zshrc ~/.zshrc_default # backup
-ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
-ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
-ln -s $DIR/zsh/zshrc ~/.zshrc
-ln -s $DIR/zsh/zpreztorc ~/.zpreztorc
-ln -s $DIR/zsh/zprofile ~/.zprofile
+mk_symlink ~/.zprezto/runcoms/zshenv ~/.zshenv
+mk_symlink ~/.zprezto/runcoms/zprofile ~/.zprofile
+mk_symlink $DIR/zsh/zshrc ~/.zshrc
+mk_symlink $DIR/zsh/zpreztorc ~/.zpreztorc
+mk_symlink $DIR/zsh/zprofile ~/.zprofile
 
 # terminal colors
-mkdir ~/.vim/colors
-ln -s ~/.dotfiles/vim/ir_black.vim ~/.vim/colors/
+mkdir ~/.vim/colors 
+mk_symlink ~/.dotfiles/vim/ir_black.vim ~/.vim/colors/
 
 # perl
-ln -s $DIR/perltidy/perltidyrc ~/.perltidyrc
+mk_symlink $DIR/perltidy/perltidyrc ~/.perltidyrc
 curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 cpanm --sudo local::lib
